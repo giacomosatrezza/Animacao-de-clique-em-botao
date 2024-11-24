6fbcf3569fbabbaaf2e55fcef8e33ebf794b6466
Giacomo Trezza <giacomosatrezza@gmail.com>
2024-11-24 14:05:28 -0300

    script pra wordpress

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 Criei essa animação em css pra simbolizar pro cliente que o botão foi clicado
+
+Script 1 - Site HTML
+Script 2 - Site Wordpress
---
+++ script2.js
@@ -0,0 +1,4 @@
+document.getElementById('comprar').addEventListener('click', function () {
+    var botaoamudar = document.querySelector('.elementor-223 .elementor-element.elementor-element-22643a2 .jet-woo-builder .single_add_to_cart_button.button');
+    botaoamudar.style.background = '#37b53c';
+});
