54088faca453ac069b08d9c47edffab15becca17
Giacomo Trezza <giacomosatrezza@gmail.com>
2024-11-24 14:11:00 -0300

    comentários no código

--- script2.js
+++ script2.js
@@ -1,4 +1,4 @@
-document.getElementById('comprar').addEventListener('click', function () {
-    var botaoamudar = document.querySelector('.elementor-223 .elementor-element.elementor-element-22643a2 .jet-woo-builder .single_add_to_cart_button.button');
-    botaoamudar.style.background = '#37b53c';
+document.getElementById('comprar').addEventListener('click', function () { //certifique que seu botão tenha esse id
+    var botaoamudar = document.querySelector('.elementor-223 .elementor-element.elementor-element-22643a2 .jet-woo-builder .single_add_to_cart_button.button'); //Mude pra classe certa do seu botão
+    botaoamudar.style.background = '#37b53c'; //Mude a cor para a cor que você deseja
 });
